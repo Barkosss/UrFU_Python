90a51e5e8ab1d2c2302e91982455896c09ce99ef
Barkosss <andrey.bar66@gmail.com>
2024-11-06 11:36:33 +0500

    fix

--- HW2/main.py
+++ HW2/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from itertools import chain
 
-def check_sex(name: str, male: bool = False, female: bool = False):
 
+def check_sex(name: str, male: bool = False, female: bool = False):
     if male:
         consonant_letters = ['б', 'в', 'г', 'д',
                              'ж', 'з', 'й', 'к',
@@ -12,10 +12,8 @@ def check_sex(name: str, male: bool = False, female: bool = False):
         additional_names = ["никита", "илья", "данила", "лёва", "алехандро"]
         exceptions_names = ["любовь"]
 
-
         return True if ((name[-1] in consonant_letters or name.lower() in additional_names)
                         and name.lower() not in exceptions_names) else False
-
 
     if female:
         vowel_letters = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
@@ -38,13 +36,13 @@ def make_stat(file_name: str) -> dict:
             # Find year
             if (start_index := line.find("<h3>")) and start_index != -1:
                 end_index = line.find("</h3")
-                year = line[start_index+4:end_index]
+                year = line[start_index + 4:end_index]
                 years_names[year] = {}
 
             # Find fullname
             if (start_index := line.find("/\">")) and start_index != -1:
                 end_index = line.find("</a")
-                full_name = line[start_index+2:end_index]
+                full_name = line[start_index + 2:end_index]
                 first_name = full_name.split(" ")[1]
                 if years_names[year].get(first_name, False):
                     years_names[year][first_name] += 1
@@ -64,7 +62,6 @@ def extract_years(stat: dict) -> list:
 
 
 def extract_general(stat: dict) -> list:
-
     """
     Функция принимает на вход вычисленную статистику и выдаёт список tuple'ов
     (имя, количество) общей статистики для всех имён.
@@ -146,7 +143,7 @@ def extract_year(stat: dict, year: str) -> list:
     stat_year = stat[year]
     extract_year_tuple = tuple(chain.from_iterable(stat_year.items()))
     extract_year_list = [(extract_year_tuple[index], int(extract_year_tuple[index + 1]))
-                        for index in range(0, len(extract_year_tuple), 2)]
+                         for index in range(0, len(extract_year_tuple), 2)]
 
     extract_year_list.sort(key=lambda x: x[1], reverse=True)
     return extract_year_list
@@ -174,7 +171,7 @@ def extract_year_male(stat: dict, year: str) -> list:
 
     year_male_tuple = tuple(chain.from_iterable(name_count.items()))
     year_male = [(year_male_tuple[index], year_male_tuple[index + 1])
-                    for index in range(0, len(year_male_tuple), 2)]
+                 for index in range(0, len(year_male_tuple), 2)]
     year_male.sort(key=lambda x: x[1], reverse=True)
     return year_male
 
@@ -201,7 +198,7 @@ def extract_year_female(stat: dict, year: str) -> list:
 
     year_female_tuple = tuple(chain.from_iterable(name_count.items()))
     year_female = [(year_female_tuple[index], year_female_tuple[index + 1])
-                    for index in range(0, len(year_female_tuple), 2)]
+                   for index in range(0, len(year_female_tuple), 2)]
     year_female.sort(key=lambda x: x[1], reverse=True)
     return year_female
 
---
+++ main.py
@@ -0,0 +1,5 @@
+for _ in range(10):
+    if _ > 5:
+        break
+else:
+    print("break")
